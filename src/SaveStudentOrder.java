7b50f67d9da9fdfd71b921ddef50e4c3dc2ce04f
Theos <theossoft@gmail.com>
2021-12-04 01:28:32 +0300

    Add Answers

--- src/SaveStudentOrder.java
+++ src/SaveStudentOrder.java
@@ -1,2 +1,18 @@
 public class SaveStudentOrder {
+    public static void main(String[] args) {
+        StudentOrder so = new StudentOrder();
+        so.hFirstName = "Алексей";
+        so.hLastName = "Петров";
+        so.wLastName = "Галина";
+        so.wFirstName = "Петрова";
+
+        long ans = saveStudentOrder(so);
+        System.out.println(ans);
+    }
+
+    static long saveStudentOrder(StudentOrder studentOrder) {
+        long answer = 199;
+        System.out.println("saveStudentOrder 1");
+        return answer;
+    }
 }
--- src/StudentOrderValidator.java
+++ src/StudentOrderValidator.java
@@ -3,26 +3,45 @@ public class StudentOrderValidator {
         checkAll();
     }
     static void checkAll() {
-        checkChildren();
-        checkCityRegister();
-        checkWedding();
-        checkStudent();
+        StudentOrder so = readStudentOrder();
+
+        AnswerCityRegister cityAnswer = checkCityRegister(so);
+        AnswerChildren childAnswer = checkChildren(so);
+        AnswerWedding wedAnswer = checkWedding(so);
+        AnswerStudent studAnswer = checkStudent(so);
+
+        sendMail(so);
     }
 
-    static void checkCityRegister() {
+    static StudentOrder readStudentOrder() {
+        StudentOrder so = new StudentOrder();
+        return so;
+    }
+
+
+    static AnswerCityRegister checkCityRegister(StudentOrder so) {
         System.out.println("CityRegister is running");
+        AnswerCityRegister ans = new AnswerCityRegister();
+        return ans;
     }
 
-    static void checkWedding() {
+    static AnswerWedding checkWedding(StudentOrder so) {
         System.out.println("Wedding is running");
+        return new AnswerWedding();
     }
 
-    static void checkChildren() {
+    static AnswerChildren checkChildren(StudentOrder so) {
         System.out.println("Children check is running");
+        return new AnswerChildren();
     }
 
-    static void checkStudent() {
+    static AnswerStudent checkStudent(StudentOrder so) {
         System.out.println("Students is running");
+        return new AnswerStudent();
+    }
+
+    static void sendMail(StudentOrder so){
+
     }
 
 }
